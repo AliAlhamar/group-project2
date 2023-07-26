c82d5b94d10776be407b62d50dcb338b7ff83de1
AliAlhamar <132816540+AliAlhamar@users.noreply.github.com>
2023-07-27 03:40:16 +0400

    code done

--- noonapp/admin.py
+++ noonapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from noonapp.models import Contact,Product
+from noonapp.models import Contact,Product,Orders,OrderUpdate
 
 # Register your models here.
 admin.site.register(Contact)
-
 admin.site.register(Product)
+admin.site.register(Orders)
+admin.site.register(OrderUpdate)
--- noonapp/views.py
+++ noonapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from noonapp.models import Contact,Product,OrderUpdate,Orders
 from django.contrib import messages
 from math import ceil
@@ -40,13 +40,42 @@ def about (request):
     return render(request,"about.html")
 
 def profile (request):
-    return render(request,"about.html")
+    return render(request,"profile.html")
 
 def checkout (request):
-    return render(request,"about.html")
+    if not request.user.is_authenticated:
+        messages.warning(request,"Login & Try Again")
+        return redirect('/auth/login')
+    if request.method=="POST":
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        amount = request.POST.get('amt')
+        email = request.POST.get('email', '')
+        address1 = request.POST.get('address1', '')
+        address2 = request.POST.get('address2','')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+        Order = Orders(items_json=items_json,name=name,amount=amount, email=email, address1=address1,address2=address2,city=city,state=state,zip_code=zip_code,phone=phone)
+        print(amount)
+        Order.save()
+        update = OrderUpdate(order_id=Order.order_id,update_desc="the order has been placed")
+        update.save()
+        thank = True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    return render(request,"checkout.html")
 
 def Products (request):
     return render(request,"Products.html")
 
 def handlerequest (request):
-    return render(request,"about.html")
+    return render(request,"handlerequest.html")
